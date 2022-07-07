0e2daff9d1af0130afb042805a1eaab225ca201f
Henry Boes <98630645+HezyHax@users.noreply.github.com>
2022-07-06 23:13:05 -0400

    Update death.js

--- death.js
+++ death.js
@@ -1,3 +1,12 @@
+'use strict';
+
+let dns = require('native-dns');
+let server = dns.createServer();
+
+server.on('listening', () => console.log('server listening on', server.address()));
+server.on('close', () => console.log('server closed', server.address()));
+server.on('error', (err, buff, req, res) => console.error(err.stack));
+server.on('socketError', (err, socket) => console.error(err));
 let authority = { address: '107.20.49.154', port: 53, type: 'udp' };
 
 function proxy(question, response, cb) {
